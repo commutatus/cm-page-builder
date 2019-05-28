26fe5e1f281a5371990a8f00e1212adda36c67ab
Vivek Srivastava <vivsri95@gmail.com>
2019-05-28 16:39:33 +0530

    added showdown

--- src/components/ContentEditable.js
+++ src/components/ContentEditable.js
@@ -3,13 +3,20 @@ import ReactDOM from 'react-dom'
 export default class ContentEditable extends React.Component{
   
   shouldComponentUpdate(nextProps){
-      return nextProps.html !== ReactDOM.findDOMNode(this).innerHTML
+    return nextProps.html !== ReactDOM.findDOMNode(this).innerHTML
+  }
+
+  onTextEdit = (e) => {
+    e.preventDefault()
+    let selection = window.getSelection()
+    console.log(selection.anchorNode)
+    let type = e.target.dataset.name
+
   }
 
   emitChange = () => {
     let node = ReactDOM.findDOMNode(this)
     var html = node.innerHTML
-    console.log(html)
     node.style = `-webkit-text-fill-color: ${html ? '#1D2129' : '#9EA0A4'}`
     if (this.props.onChange && html !== this.lastHtml) {
       this.props.onChange({
@@ -25,15 +32,22 @@ export default class ContentEditable extends React.Component{
   render() {
     const { placeholder, className, styles } = this.props
     return(
-      <div 
-        className={className}
-        onInput={this.emitChange} 
-        onBlur={this.emitChange}
-        contentEditable
-        placeholder={placeholder}
-        dangerouslySetInnerHTML={{__html: this.props.html}}
-        styles={styles}
-      />
+      <div>
+        <div 
+          className={className}
+          onInput={this.emitChange} 
+          onBlur={this.emitChange}
+          contentEditable
+          placeholder={placeholder}
+          dangerouslySetInnerHTML={{__html: this.props.html}}
+          styles={styles}
+        />
+        <div>
+          <div data-name="bold" onClick={this.onTextEdit}>Bold</div>
+          <div data-name="italics" onClick={this.onTextEdit}>Italics</div>
+          <div data-name="strike-through" onClick={this.onTextEdit}>Strike Through</div>
+        </div>
+      </div>
     )
   }
 }
