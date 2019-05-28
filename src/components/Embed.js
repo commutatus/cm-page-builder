4e34ea0f43628d8386e6f920546077ecea2f28f9
Vivek Srivastava <vivsri95@gmail.com>
2019-05-28 14:39:21 +0530

    add upload and embed component.

---
+++ src/components/Embed.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export class Embed extends React.Component{
+
+  render(){
+    return(
+      <div className="embed-container">
+        Paste the url from Vimeo or Youtube.
+      </div>
+    )
+  }
+}
---
+++ src/components/Upload.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export class Upload extends React.Component{
+
+  render(){
+    return(
+      <div className="upload-container">
+        Click to upload Image or Video.
+      </div>
+    )
+  }
+}
