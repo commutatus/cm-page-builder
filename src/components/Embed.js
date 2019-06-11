5157cad82b230cc33c874a7c047dcd9863c5934f
barath <barath@commutatus.com>
2019-06-11 15:30:35 +0530

    merge conflict fixed

--- src/components/Embed.js
+++ src/components/Embed.js
@@ -11,46 +11,17 @@ const WrappedEmbed = (props) => {
         ?
         <iframe title="video-frame" width="100%" height="320px" src={videoUrl} />
         :
-        <input 
-          placeholder="embed"
-          onBlur={props.handleEmbed}
-        />
+        <React.Fragment>
+          <span className="embed-icon"><i className="cm-video" /></span>
+          <input 
+            placeholder="Paste the URL from Vimeo or YouTube"
+            className="embed-input"
+            onBlur={this.handleChange}
+          />
+        </React.Fragment>
       }
     </div>
   )
 }
 
-  state = {
-    videoUrl: this.props.url || ''
-  }
-
-  handleChange = (e) => {
-    this.setState({
-      videoUrl: getVideoUrl(e.target.value), 
-    }, () => {
-      this.props.handleUpdate({content: this.state.html, id: this.props.id}, 'Embed')
-    })
-  }
-
-  render(){
-    let {videoUrl} = this.state
-    return(
-      <div className="cm-embed">
-        {
-          videoUrl 
-          ?
-          <iframe title="video-frame" width="100%" height="320px" src={this.state.videoUrl} />
-          :
-          <React.Fragment>
-            <span className="embed-icon"><i className="cm-video" /></span>
-            <input 
-              placeholder="Paste the URL from Vimeo or YouTube"
-              className="embed-input"
-              onBlur={this.handleChange}
-            />
-          </React.Fragment>
-        }
-      </div>
-    )
-  }
-}
+export const Embed = withComponent(WrappedEmbed)
