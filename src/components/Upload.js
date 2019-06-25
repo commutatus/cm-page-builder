fb9e2e3b6d918d435dd8bf7eca1260d1ee672129
surendhar95 <esurendhar95@gmail.com>
2019-06-25 14:12:08 +0530

    refactor(api updates): Changes in api updates for pagfe components

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -15,35 +15,39 @@ class WrappedUpload extends React.Component{
     this.state = {}
   }
 
-  uploadImage = (e) => {
+  uploadImage = (e, context) => {
     var content = ''
     if (e.target.files && e.target.files[0]) {
       let fileName = e.target.files[0].name;
       let reader = new FileReader();
       reader.onload = (e) => {
         content = e.target.result;
-        this.props.updateComponent({id: this.props.id, newState: {image: {filename: fileName, content}}})
+        if (this.props.initial) 
+          context.emitUpdate(null, {client_reference_id: this.props.id,  component_attachment: { filename: fileName, content }, position: this.props.position, component_type: this.props.componentType}, 'createComponent')
+        else
+         context.emitUpdate(this.props.id, { component_attachment: { filename: fileName, content }, position: this.props.position, component_type: this.props.componentType }, 'updateComponent')
+        this.props.updateComponent({id: this.props.id, newState: {component_attachment: {filename: fileName, content}, initial: false}})
       }
       reader.readAsDataURL(e.target.files[0]);
     }
   }
 
   render() {
-    let {image} = this.props
+    let {component_attachment} = this.props
     return(
       <PermissionContext.Consumer>
         {
           value => 
           <div className={`component-section cm-uploader ${value.status.toLowerCase()}`} onClick={() => this.fileInputElem && this.fileInputElem.click()}>
             {
-              image
+              component_attachment
               ?
-              <img src={image.url || image.content} width="100%" height="400px" alt={image.filename} />
+              <img src={component_attachment.url || component_attachment.content} width="100%" height="400px" alt={component_attachment.filename} />
               :
               <React.Fragment>
                 <span><i className="cm-upload" /></span>
                 Click to upload file
-                <input ref={node => this.fileInputElem = node} type="file" accept="image/*" hidden onChange={this.uploadImage} />
+                <input ref={node => this.fileInputElem = node} type="file" accept="image/*" hidden onChange={(e) => this.uploadImage(e, value)} />
               </React.Fragment>
             }
           </div>
