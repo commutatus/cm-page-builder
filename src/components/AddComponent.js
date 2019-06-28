1472827d8606a77ade2659bf4622247fb7d8edc1
Vivek Srivastava <vivsri95@gmail.com>
2019-06-28 13:51:29 +0530

    fix(page): navigation fixes for non text component

--- src/components/AddComponent.js
+++ src/components/AddComponent.js
@@ -118,8 +118,11 @@ class AddComponent extends React.Component{
         computedStyles = window.getComputedStyle(e.target)
         elemPad = computedStyles.getPropertyValue("padding-top").replace('px', '')
         elemMar = computedStyles.getPropertyValue('margin-top').replace('px', '')
-        extraHeight = (+elemPad) + (+elemMar)
-        if((caretRect.x === 0 && caretRect.y === 0) || elemRect.top === (caretRect.top - extraHeight + 1)){
+        extraHeight = (+elemPad) 
+        if((caretRect.x === 0 && caretRect.y === 0) ||  // when no text is there
+            (elemRect.top === (caretRect.top - extraHeight)) || // is not a text component
+            (elemRect.top === (caretRect.top - extraHeight - 1)) // is a text component
+        ){
           e.preventDefault()
           currentElemPos = appData.componentData.findIndex(data => data.id === currentElem.elemId)
           while(currentElemPos > 0){
@@ -137,8 +140,11 @@ class AddComponent extends React.Component{
           computedStyles = window.getComputedStyle(e.target)
           elemPad = computedStyles.getPropertyValue("padding-bottom").replace('px', '')
           elemMar = computedStyles.getPropertyValue('margin-bottom').replace('px', '')
-          extraHeight = (+elemPad) + (+elemMar)
-          if((caretRect.x === 0 && caretRect.y === 0) || elemRect.bottom === (caretRect.bottom + extraHeight - 1)){
+          extraHeight = (+elemPad)
+          if((caretRect.x === 0 && caretRect.y === 0) || 
+              (elemRect.bottom === (caretRect.bottom - extraHeight)) ||
+              (elemRect.bottom === (caretRect.bottom + extraHeight + 1))
+          ){
             e.preventDefault()
             currentElemPos = appData.componentData.findIndex(data => data.id === currentElem.elemId)
             while(currentElemPos < appData.componentData.length - 1){
