d7ba8240185a2019de9fb3a8392fd77ecc62bbff
Chandu <chandan@commutatus.com>
2020-01-23 12:55:26 +0530

    fix(router): remove router-dom

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import '../styles/components/Upload.css';
 import withComponent from './withComponent';
 import { PermissionContext } from '../contexts/permission-context';
